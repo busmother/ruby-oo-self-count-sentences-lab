47a7b7ce4563e12a092d6cb25f5a894d8322310b
Ian Marshall <imarshall14@gmail.com>
2020-12-19 17:46:43 -0500

    commit tree 0db2ed717e774303c9244d4f1bdc125c59165fa7

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    include?(".") ? true : false
   end
 
   def question?
-
+    include?("?") ? true : false
   end
 
   def exclamation?
-
+    include?("!") ? true : false
   end
 
   def count_sentences
-
+    split("!").join("?").split("?").join(".").split(".").delete_if {|i| i == ""}.count
   end
+
 end
